5164267c8721fbf438b34e4cf1395d26494813c2
Shailesh Patil <shaileshp@gmail.com>
2023-04-27 16:14:41 +0100

    TAV-952 updated the test coverage

--- test/uk/gov/hmrc/ninoinsightsstubs/controllers/NinoRiskListsControllerSpec.scala
+++ test/uk/gov/hmrc/ninoinsightsstubs/controllers/NinoRiskListsControllerSpec.scala
@@ -56,5 +56,16 @@ class NinoRiskListsControllerSpec extends AnyWordSpec with Matchers {
       status(result) shouldBe Status.OK
       contentAsString(result) shouldBe responseFalse
     }
+    "return 400 " in {
+      val rejectNinoDetails: NinoDetails = NinoDetails("AB123456A")
+      val fakeRequestWithFalse = FakeRequest("POST", "/reject/nino")
+        .withBody(Json.toJson(""))
+        .withHeaders(CONTENT_TYPE -> "application/json")
+      val responseFalse = """{"result": false}"""
+
+      val result = controller.isNinoOnRejectList()(fakeRequestWithFalse)
+      status(result) shouldBe Status.BAD_REQUEST
+    }
+
   }
 }
